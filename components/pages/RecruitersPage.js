aaee1605268c253a1e2ff1f029b431bf50c5a95f
gokulrajr-r <gokul@rareminds.in>
2025-10-13 08:42:27 +0000

    auto-commit for e33110d7-e5d7-4df8-9439-b72e5240d2eb

--- components/pages/RecruitersPage.js
+++ components/pages/RecruitersPage.js
@@ -335,7 +335,8 @@ export default function RecruitersPage({ currentUser }) {
             ) : (
               <p className="text-center text-muted-foreground py-8">No recruiters found</p>
             )}
-          </div>
+            </div>
+          )}
         </CardContent>
       </Card>
 
